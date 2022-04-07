41835d7380ff86a9e72194580ef8f11ed8bc401e
aesop-igaw <aesop.kim@igaworks.com>
2022-04-07 18:01:42 +0900

    [feat] 사이드바 구현

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,219 @@
+import { motion, useAnimation } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { AiFillFire, AiFillMessage, AiOutlineMenuFold, AiOutlineMenuUnfold } from 'react-icons/ai';
+import {
+  BsBookmarkFill,
+  BsEyeFill,
+  BsFillPersonFill,
+  BsGithub,
+  BsLinkedin,
+  BsSearch,
+} from 'react-icons/bs';
+import { FaCog } from 'react-icons/fa';
+import { IoMdArrowRoundUp } from 'react-icons/io';
+import { MdNightlightRound } from 'react-icons/md';
+
+const data = [
+  {
+    name: 'HTML﹒CSS﹒JS',
+    items: [
+      {
+        title: 'Popular',
+        icon: AiFillFire,
+      },
+      {
+        title: 'Most Upvoted',
+        icon: IoMdArrowRoundUp,
+      },
+      {
+        title: 'Best Discussions',
+        icon: AiFillMessage,
+      },
+      {
+        title: 'Search',
+        icon: BsSearch,
+      },
+    ],
+  },
+  {
+    name: 'React',
+    items: [
+      {
+        title: 'Bookmarks',
+        icon: BsBookmarkFill,
+      },
+      {
+        title: 'Reading history',
+        icon: BsEyeFill,
+      },
+      {
+        title: 'Focus Mode',
+        icon: MdNightlightRound,
+      },
+      {
+        title: 'Customize',
+        icon: FaCog,
+      },
+    ],
+  },
+];
+
+const datafooter = [
+  {
+    name: 'Contact',
+    items: [
+      {
+        title: 'Blog',
+        icon: BsBookmarkFill,
+      },
+      {
+        title: 'Github',
+        icon: BsGithub,
+      },
+      {
+        title: 'LinkedIn',
+        icon: BsLinkedin,
+      },
+      {
+        title: 'Portfolio',
+        icon: BsFillPersonFill,
+      },
+    ],
+  },
+];
+
+export default function Home() {
+  const [active, setActive] = useState(false);
+  const controls = useAnimation();
+  const controlTitle = useAnimation();
+  const controlText = useAnimation();
+  const controlTitleText = useAnimation();
+
+  const showMore = () => {
+    controls.start({
+      width: '250px',
+      transition: { duration: 0.001 },
+    });
+    controlTitle.start({
+      opacity: 1,
+      display: 'block',
+      transition: { delay: 0.3 },
+    });
+    controlText.start({
+      opacity: 1,
+      display: 'block',
+      transition: { delay: 0.3 },
+    });
+    controlTitleText.start({
+      opacity: 1,
+      transition: { delay: 0.3 },
+    });
+
+    setActive(true);
+  };
+
+  const showLess = () => {
+    controls.start({
+      width: '55px',
+      transition: { duration: 0.001 },
+    });
+    controlTitle.start({
+      opacity: 0,
+      display: 'none',
+    });
+    controlText.start({
+      opacity: 0,
+      display: 'none',
+    });
+
+    controlTitleText.start({
+      opacity: 0,
+    });
+
+    setActive(false);
+  };
+
+  useEffect(() => {
+    showMore();
+  }, []);
+
+  return (
+    <motion.aside
+      animate={controls}
+      className="max-w-[250px] dark:bg-slate-900 animate duration-300 border-r border-gray-700 relative flex flex-col pb-10 min-h-screen whitespace-nowrap h-screen overflow-auto"
+    >
+      {/* 타이틀 */}
+      <div className="sticky top-0">
+        <h2 className="font-thick text-white text-2xl flex items-center p-4 pb-2 dark:bg-slate-900">
+          <span>
+            {active && (
+              <AiOutlineMenuFold
+                onClick={showLess}
+                className="text-2xl text-white cursor-pointer"
+              />
+            )}
+            {!active && (
+              <AiOutlineMenuUnfold
+                onClick={showMore}
+                className="text-2xl text-white cursor-pointer"
+              />
+            )}
+          </span>
+          {
+            <motion.span animate={controlTitle} className="ml-2">
+              Maeng&apos;s Exercise
+            </motion.span>
+          }
+        </h2>
+        <div className="h-4 bg-gradient-to-b from-white dark:from-slate-900 sticky top-200"></div>
+      </div>
+      <div className="grow">
+        {data.map((group, index) => (
+          <ul key={index} className="my-2">
+            {/* 메뉴 그룹 */}
+            <motion.p
+              animate={controlTitleText}
+              className="mb-2 ml-4 text-sm font-bold text-gray-100 whitespace-nowrap"
+            >
+              {group.name}
+            </motion.p>
+            {/* 메뉴 */}
+            {group.items.map((item, index2) => (
+              <li key={index2} className="group flex px-4 py-1 cursor-pointer">
+                <item.icon className="text-lg text-gray-500 group-hover:text-white" />
+                <motion.p
+                  animate={controlText}
+                  className="ml-4 text-sm font-bold text-gray-400 group-hover:text-white"
+                >
+                  {item.title}
+                </motion.p>
+              </li>
+            ))}
+          </ul>
+        ))}
+      </div>
+      {/* 하단 */}
+      <div>
+        {datafooter.map((group, index) => (
+          <ul key={index} className="my-2">
+            <motion.p animate={controlTitleText} className="mb-2 ml-4 text-sm font-bold text-white">
+              {group.name}
+            </motion.p>
+
+            {group.items.map((item, index2) => (
+              <li key={index2} className="group flex px-4 py-1 cursor-pointer">
+                <item.icon className="text-lg text-gray-500 group-hover:text-white" />
+                <motion.p
+                  animate={controlText}
+                  className="ml-4 text-sm font-bold text-gray-400 group-hover:text-white"
+                >
+                  {item.title}
+                </motion.p>
+              </li>
+            ))}
+          </ul>
+        ))}
+      </div>
+    </motion.aside>
+  );
+}
